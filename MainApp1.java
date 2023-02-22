5cc0109d53f11c45bfdebc7364cd7123c5a1a1a3
VijaySimhaRedd2 <vijay2161909@gmail.com>
2023-02-22 09:11:29 +0530

    First Commit

---
+++ MainApp1.java
@@ -0,0 +1,10 @@
+package in.ineuron.main1;
+
+public class MainApp1 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Eclepse IDE....");
+		System.out.println("Hey inside m1()");
+	}
+
+}
